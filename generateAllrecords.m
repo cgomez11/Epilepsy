4ac87788bbde8cdb8da4d9b20ec1ba7f0e89e608
cgomez11 <c.gomez10@uniandes.edu.co>
2017-11-09 19:19:26 -0500

    Update generateAllrecords.m

--- generateAllrecords.m
+++ generateAllrecords.m
@@ -1,17 +1,21 @@
 %extraer registros completos de pacientes de test y sus etiquetas
-datasetPath = '/hpcfs/home/c.gomez10/inv/Dataset';
+%datasetPath = '/hpcfs/home/c.gomez10/inv/Dataset';
+datasetPath = '/media/user_home2/EEG/Epilepsy'
 dirDataset = dir(datasetPath);
 load('AllTimes_cut.mat');
 load('regNumber_cut.mat');
-test_patients = [16, 14, 19, 17, 13, 1, 24, 15];
+%test_patients = [16, 14, 19, 17, 13, 1, 24, 15];
+%test_patients = [1];
 
 %for id = 1:numel(test_patients) %recorrer pacientes
-for id = 3:numel(dirDataset) %recorrer pacientes   
+%for id = 3:numel(dirDataset)%recorrer pacientes   
+for id =3:3
     pat_labels = {};
     pat_records = {};
     patient = dirDataset(id).name;
     patientPath = fullfile(datasetPath,patient);
-    addpath(sprintf('/hpcfs/home/c.gomez10/inv/Dataset/%s',patient))
+    %addpath(sprintf('/hpcfs/home/c.gomez10/inv/Dataset/%s',patient))
+    addpath(sprintf('/media/user_home2/EEG/Epilepsy/%s',patient))
     
     dataTimesPatient = allTimes{id-2};
     regNum = regNumber(:,id-2);
@@ -30,7 +34,7 @@ for id = 3:numel(dirDataset) %recorrer pacientes
            
         else
             timesPatient = dataTimesPatient{j}*s_SRate; 
-            %crear señal binaria con 1's y 0's
+            %crear seÃ±al binaria con 1's y 0's
             m_SignalsBinary = m_SignalsMat;
             for i =1:2:numel(timesPatient)
                 m_SignalsBinary(:,timesPatient(i):timesPatient(i+1))=1; %marcar periodos de crisis con 1's
