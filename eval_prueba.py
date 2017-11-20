af78dfd23989d73de9aaf3f4a80b330f01ace14e
Jorge Madrid <ja.madrid2714@uniandes.edu.co>
2017-11-20 15:02:22 -0500

    concatenates .npys in order

--- eval_prueba.py
+++ eval_prueba.py
@@ -1,37 +1,52 @@
 # Code that evaluates precision and recall for cross-validation experiment.
 # It covers all prediction*.npy files and compares them element-wise with their corresponding target*.npy
 # The code prints out stats for that experiment.
-# Prediction must be discrete at this point.
+# Prediction must be discrete at this point. 
+###### Changes mst be made to accomodate iteration over confidence
 
 import os
 import numpy as np
 import scipy as sp
 import scipy.signal
 
+# Constants
+pat_num = 21
+
+# Gets record names to open preds and targets simultaneosuly
+R = []
+with open("stats.txt") as st:
+
+	for num, line in enumerate(st):
+		
+		RecordN, Record, SpecificityN, Specificity, RecallN, Recall, PrecisionN, Precision = line.split (' ')
+		R.append(Record)
+    
+# Opens preds and target files in order. Loads into arrays.
+# Concatenates into single one-dimensional array           
 p = []
 t = []
 
-list_of_files = os.listdir(os.getcwd()) #list of files in the current directory
-for each_file in list_of_files:
-    if each_file.startswith('pred'):  #since its all type str you can simply use startswith
-        pi = np.load(each_file)
-        #print(len(pi))
-        p = np.concatenate((p, pi))
+for i in range(len(R)):
+	filename_pred = 'pred_' + R[i] + '_21.npy'
+	filename_target = 'target_' + R[i] + '_21.npy'
 
-    elif each_file.startswith('target'):
-    	ti = np.load(each_file)
-    	#print(len(ti))
-    	t = np.concatenate((t, ti))
+	pi = np.load(filename_pred)
+	p = np.concatenate((p, pi))
 
-m = 121
-p_m = sp.signal.medfilt(p, [m])
+	ti = np.load(filename_target)
+	t = np.concatenate((t, ti))
 
+# Makes sure both preds and targets have the same size
+print(p.size)
+print(t.size)
+
+# Counts True Positives and Negatives; and False Positives and Negatives
 TN = 0
 TP = 0
 FN = 0
 FP = 0
 
-for i in range(p.size):
+for i in range(len(p)-1):
 	if p[i] == 0 and t[i] == 0:
 		TN += 1
 	elif p[i] == 0 and t[i] == 1:
@@ -41,7 +56,36 @@ for i in range(p.size):
 	elif p[i] == 1 and t[i] == 1:
 		TP += 1
 
+# Computes Precision and Recall
 precision = TP/(TP + FP)
 recall = TP/(TP + FN)
 
+print('TN =', TN, 'TP =', TP, 'FN =', FN, 'FP =', FP, 'precision=', precision, 'recall', recall)
+
+#########
+### Filtering to supress false positives
+##### Same as before but for filtered signal
+#########
+m = 61
+p_m = sp.signal.medfilt(p, [m])
+
+TN = 0
+TP = 0
+FN = 0
+FP = 0
+
+for i in range(p_m.size):
+	if p_m[i] == 0 and t[i] == 0:
+		TN += 1
+	elif p_m[i] == 0 and t[i] == 1:
+		FN += 1
+	elif p_m[i] == 1 and t[i] == 0:
+		FP += 1
+	elif p_m[i] == 1 and t[i] == 1:
+		TP += 1
+
+
+precision = TP/(TP + FP)
+recall = TP/(TP + FN)
+print('median filter=', m)
 print('TN =', TN, 'TP =', TP, 'FN =', FN, 'FP =', FP, 'precision=', precision, 'recall', recall)
